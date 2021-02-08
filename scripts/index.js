9268636e55a521f4b40d048e092d7e404a132b5b
RomanBabskikh <65495095+RomanBabskikh@users.noreply.github.com>
2021-02-08 17:14:18 +0300

    сделал второй попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const popup = document.querySelector('.popup');
+const popupFoto = document.querySelector('.popup__foto');
 const editButton = document.querySelector('.profile__button');
 const editButton2 = document.querySelector('.profile__add-button');
 const popupCloseButton = document.querySelector('.popup__close');
@@ -14,19 +15,30 @@ function showPopup(){
     nameField.value = title.textContent;
     infoField.value = subTitle.textContent;
 }
-
-
 editButton.addEventListener('click' , showPopup);
-editButton2.addEventListener('click' , showPopup);
 
+
+//функция по открытию попапа 2//
+function showPopupFoto(){
+    popupFoto.classList.add('popup_opened');
+}
+editButton2.addEventListener('click' , showPopupFoto);
 
 
 //функция по закрытию попапа//
 function closePopup(){
     popup.classList.remove('popup_opened');
 }
-
 popupCloseButton.addEventListener('click' , closePopup);
+
+//функция по закрытию попапа 2//
+function closePopupFoto(){
+    popupFoto.classList.remove('popup_opened');
+}
+popupFotoCloseButton.addEventListener('click' , closePopupFoto);
+
+
+
 
 
 //функция по форме//
